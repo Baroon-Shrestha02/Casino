6d78b0fa3f3ee0596bda2843308b37968e801cbd
Baroon-Shrestha <bar00nshrestha098@gmail.com>
2025-09-21 16:00:05 +0545

    Updated

---
+++ src/Components/ContactComponents/ContactMain.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ContactHero from "./ContactHero";
+import ContactForm from "./ContactForm";
+
+export default function ContactMain() {
+  return (
+    <div>
+      <ContactHero />
+      <ContactForm />
+    </div>
+  );
+}
